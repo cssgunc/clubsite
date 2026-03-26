2fee0008b58b6e0bf33f0f922d5982b69fadaa7a
Daniel Kane <dakane@unc.edu>
2026-03-26 15:32:21 -0400

    added Parr Center and CCC to past projects on website

--- src/app/components/PastProjects.tsx
+++ src/app/components/PastProjects.tsx
@@ -51,6 +51,24 @@ export const PastProjects = () => {
     />
   );
 
+  const cccTitle = (
+    <ProjectTitle
+      href="https://campusandcommunitycoalition.org"
+      logoSrc="/ccc_logo.png"
+      alt="Campus and Community Coalition"
+      title="Campus and Community Coalition"
+    />
+  );
+  const parrCenterTitle = (
+    <ProjectTitle
+      href="https://ethicsbowlacademy.org/homepage"
+      logoSrc="/parr_center_logo_2.jpg"
+      alt="Parr Center"
+      title="Parr Center Ethics Bowl Academy"
+    />
+  );
+
+
   return (
     <div className="bg-white w-full rounded-lg p-6 md:p-8 text-black flex flex-col">
       <div className="flex flex-row justify-between items-center">
@@ -209,6 +227,53 @@ export const PastProjects = () => {
             height={700}
             className="border-2 border-gray-300 rounded-lg"
           />
+        </CollapsibleSection>
+        <CollapsibleSection
+          title={cccTitle}
+          titleClassName="text-2xl min-w-fit font-geist-mono"
+          defaultExpanded={false}
+          href="https://campusandcommunitycoalition.org"
+        >
+          <p className="text-sm text-gray-500">
+            The Campus & Community Coalition (CCC) is a collaborative force 
+            bringing together university and community partners to address the
+            harms associated with high-risk drinking. By fostering open dialogue,
+            sharing power, and using data-driven strategies, they work to create
+            an environment where everyone can thrive socially, academically,
+            and economically. This website will be used primarily to display 
+            important trends regarding alcohol use in the Chapel Hill community 
+            alongside other important resources.
+          </p>
+            <Image
+              src="/ccc_preview.png"
+              alt="Campus and Community Coalition"
+              width={700}
+              height={700}
+              className="border-2 border-gray-300 rounded-lg"
+            />
+        </CollapsibleSection>
+
+        <CollapsibleSection
+          title={parrCenterTitle}
+          titleClassName="text-2xl min-w-fit font-geist-mono"
+          defaultExpanded={false}
+          href="https://ethicsbowlacademy.org/homepage"
+        >
+          <p className="text-sm text-gray-500">
+            An Ethics Bowl Academy website for the Parr Center for Ethics,
+            allowing them to help students build critical thinking and 
+            ethical reasoning skills through interactive modules. Module features 
+            include video lessons, knowledge quizzes, digital flashcards, 
+            reflective writing prompts, drag-and-drop sorting questions, and public polls. 
+
+          </p>
+            <Image
+              src="/parrcenter_homepage.png"
+              alt="Parr Center Ethics Bowl Academy"
+              width={700}
+              height={700}
+              className="border-2 border-gray-300 rounded-lg"
+            />
         </CollapsibleSection>
       </motion.div>
 
